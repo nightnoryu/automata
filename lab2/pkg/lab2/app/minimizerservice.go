df3d483594d0608209167e2187b64b85a96cb3f4
m3tro1d <qazwf734@gmail.com>
2022-10-13 14:50:25 +0300

    lab2: add equivalency groups division for moore

--- lab2/pkg/lab2/app/minimizerservice.go
+++ lab2/pkg/lab2/app/minimizerservice.go
@@ -1,5 +1,10 @@
 package app
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func NewMinimizerService(inputOutputAdapter InputOutputAdapter) *MinimizerService {
 	return &MinimizerService{
 		inputOutputAdapter: inputOutputAdapter,
@@ -27,7 +32,83 @@ func (s *MinimizerService) MinimizeMoore(inputFilename, outputFilename string) e
 		return err
 	}
 
-	// TODO
+	stateToGroupMap, groupAmount := buildZeroEquivalencyGroups(mooreAutomaton.StateSignals)
+	for {
+		previousGroupAmount := groupAmount
+
+		stateToGroupMap, groupAmount = buildNextEquivalencyGroups(
+			stateToGroupMap,
+			mooreAutomaton.InputSymbols,
+			mooreAutomaton.Moves,
+		)
+
+		if previousGroupAmount == groupAmount {
+			break
+		}
+	}
+
+	fmt.Println(stateToGroupMap, groupAmount)
 
 	return s.inputOutputAdapter.WriteMoore(outputFilename, mooreAutomaton)
+}
+
+func buildZeroEquivalencyGroups(stateSignals map[string]string) (stateToGroupMap map[string]int, groupAmount int) {
+	signalToStatesMap := make(map[string][]string)
+	for state, signal := range stateSignals {
+		signalToStatesMap[signal] = append(signalToStatesMap[signal], state)
+	}
+
+	stateToGroupMap = make(map[string]int)
+	for _, states := range signalToStatesMap {
+		for _, state := range states {
+			stateToGroupMap[state] = groupAmount
+		}
+		groupAmount++
+	}
+
+	return stateToGroupMap, groupAmount
+}
+
+func buildNextEquivalencyGroups(
+	stateToGroupMap map[string]int,
+	inputSymbols []string,
+	moves map[InitialStateAndInputSymbol]string,
+) (stateToNewGroupMap map[string]int, groupAmount int) {
+	groupToStatesMap := make(map[int][]string)
+	for state, group := range stateToGroupMap {
+		groupToStatesMap[group] = append(groupToStatesMap[group], state)
+	}
+
+	stateToNewGroupMap = make(map[string]int)
+	for _, groupStates := range groupToStatesMap {
+		stateToGroupHashMap := make(map[string]string)
+
+		for _, sourceState := range groupStates {
+			for _, inputSymbol := range inputSymbols {
+				key := InitialStateAndInputSymbol{
+					State:  sourceState,
+					Symbol: inputSymbol,
+				}
+
+				destinationState := moves[key]
+				destinationGroup := stateToGroupMap[destinationState]
+
+				stateToGroupHashMap[sourceState] += strconv.Itoa(destinationGroup)
+			}
+		}
+
+		groupHashToStatesMap := make(map[string][]string)
+		for state, groupHash := range stateToGroupHashMap {
+			groupHashToStatesMap[groupHash] = append(groupHashToStatesMap[groupHash], state)
+		}
+
+		for _, newStates := range groupHashToStatesMap {
+			for _, state := range newStates {
+				stateToNewGroupMap[state] = groupAmount
+			}
+			groupAmount++
+		}
+	}
+
+	return stateToNewGroupMap, groupAmount
 }
