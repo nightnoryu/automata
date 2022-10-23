92cacfba675c4153a5e65e78fb44983de93e1c4f
m3tro1d <qazwf734@gmail.com>
2022-10-23 14:50:52 +0300

    lab3: add template

--- lab3/cmd/lab3/main.go
+++ lab3/cmd/lab3/main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"log"
+
+	"automata/common/infrastructure"
+	"automata/lab3/pkg/lab3/app"
+)
+
 func main() {
+	args, err := parseArgs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	adapter := infrastructure.NewInputOutputAdapter()
+	service := app.NewDeterminatorService(adapter)
+
+	if err = service.Determinate(args.InputFilename, args.OutputFilename); err != nil {
+		log.Fatal(err)
+	}
 }
--- lab3/pkg/lab3/app/determinatorservice.go
+++ lab3/pkg/lab3/app/determinatorservice.go
@@ -1,1 +1,20 @@
 package app
+
+import "automata/common/app"
+
+const newStatesIdentifier = "S"
+
+func NewDeterminatorService(inputOutputAdapter app.InputOutputAdapter) *DeterminatorService {
+	return &DeterminatorService{
+		inputOutputAdapter: inputOutputAdapter,
+	}
+}
+
+type DeterminatorService struct {
+	inputOutputAdapter app.InputOutputAdapter
+}
+
+func (s *DeterminatorService) Determinate(inputFilename, outputFilename string) error {
+	// TODO
+	return nil
+}
