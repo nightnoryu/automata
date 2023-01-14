f4f521e00f1c4376296c5212b64c3173c6978522
m3tro1d <qazwf734@gmail.com>
2023-01-14 17:58:32 +0300

    lab3 task2: Implement closures building

--- lab3/task2/pkg/task2/app/determinatorservice.go
+++ lab3/task2/pkg/task2/app/determinatorservice.go
@@ -1,10 +1,11 @@
 package app
 
 import (
-	"fmt"
-
 	"automata/common/app"
+	"fmt"
 )
+
+const emptyMoveIndicator = "e"
 
 func NewDeterminatorService(inputOutputAdapter app.FiniteInputOutputAdapter) *DeterminatorService {
 	return &DeterminatorService{
@@ -22,7 +23,55 @@ func (s *DeterminatorService) Determinate(inputFilename, outputFilename string) 
 		return err
 	}
 
-	fmt.Println(automaton)
+	closures := buildClosures(automaton)
+	fmt.Println(closures)
 
 	return nil
+}
+
+func buildClosures(automaton app.NonDeterministicFiniteAutomaton) map[string][]string {
+	result := make(map[string][]string)
+	for _, state := range automaton.States {
+		key := app.InitialStateAndInputSymbol{
+			State:  state,
+			Symbol: emptyMoveIndicator,
+		}
+
+		for _, destinationState := range automaton.Moves[key] {
+			result[state] = append(result[state], destinationState)
+		}
+	}
+
+	for recurseClosures(result) {
+	}
+
+	return result
+}
+
+func recurseClosures(result map[string][]string) bool {
+	foundDeeper := false
+	for state, closure := range result {
+		for _, closureState := range closure {
+			for _, transitiveState := range result[closureState] {
+				if inSlice(result[state], transitiveState) {
+					continue
+				}
+
+				result[state] = append(result[state], transitiveState)
+				foundDeeper = true
+			}
+		}
+	}
+
+	return foundDeeper
+}
+
+func inSlice(haystack []string, needle string) bool {
+	for _, value := range haystack {
+		if value == needle {
+			return true
+		}
+	}
+
+	return false
 }
